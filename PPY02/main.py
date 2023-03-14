9153fd3b137ef216cdeb9fcce9bacfcf11e1db22
s22451 <s22451@pjwstk.edu.pl>
2023-03-14 13:54:37 +0100

    odkomentowanie kodu w rozwiązaniu zadania

--- PPY02/main.py
+++ PPY02/main.py
@@ -1,45 +1,45 @@
 # zadanie 1
 
-# a = "Python 2023"
-# b = "Python 2023"
-# c = "Python 2023"
-#
-# print(id(a) == id(b))
-# print(id(b) == id(c))
-#
-# print(hex(id(a)))
-# print(type(a))
-# print(hex(id(b)))
-# print(type(b))
-# print(hex(id(c)))
-# print(type(c))
-#
-# c = "Java 11"
-#
-# print(id(a) == id(b))
-# print(id(b) == id(c))
-#
-# print(hex(id(a)))
-# print(type(a))
-# print(hex(id(b)))
-# print(type(b))
-# print(hex(id(c)))
-# print(type(c))
+a = "Python 2023"
+b = "Python 2023"
+c = "Python 2023"
+
+print(id(a) == id(b))
+print(id(b) == id(c))
+
+print(hex(id(a)))
+print(type(a))
+print(hex(id(b)))
+print(type(b))
+print(hex(id(c)))
+print(type(c))
+
+c = "Java 11"
+
+print(id(a) == id(b))
+print(id(b) == id(c))
+
+print(hex(id(a)))
+print(type(a))
+print(hex(id(b)))
+print(type(b))
+print(hex(id(c)))
+print(type(c))
 
 # zadanie 2
 
-# i = int(input("Podaj pierwszą liczbe: "))
-# j = int(input("Podaj drugą liczbe: "))
-# z = input("podaj operator (+,-,/,*): ")
-#
-# if z == '+':
-#     print(i+j)
-# elif z == '-':
-#     print(i-j)
-# elif z == '/':
-#     print(i/j)
-# elif z == '*':
-#     print(i*j)
+i = int(input("Podaj pierwszą liczbe: "))
+j = int(input("Podaj drugą liczbe: "))
+z = input("podaj operator (+,-,/,*): ")
+
+if z == '+':
+    print(i+j)
+elif z == '-':
+    print(i-j)
+elif z == '/':
+    print(i/j)
+elif z == '*':
+    print(i*j)
 
 # zadanie 3
 
